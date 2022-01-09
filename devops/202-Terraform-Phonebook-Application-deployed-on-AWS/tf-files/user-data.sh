e7766907c01229e4536c0ef531ddf61d793c2507
Mrt-GitHub <bern.murat@gmail.com>
2022-01-09 01:44:12 +0100

    main.tf, user-data and phonebook.py modified

--- devops/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/user-data.sh
+++ devops/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/user-data.sh
@@ -5,4 +5,4 @@ pip3 install flask
 pip3 install flask_mysql
 yum install git -y
 cd /home/ec2-user && git clone https://github.com/Mrt-GitHub/My-Projects.git && rm -r My-Projects/!(devops)
-python3 /home/ec2-user/devops/202-Terraform-Phonebook-Application-deployed-on-AWS/phonebook-app.py
+python3 /home/ec2-user/My-Projects/devops/202-Terraform-Phonebook-Application-deployed-on-AWS/phonebook-app.py

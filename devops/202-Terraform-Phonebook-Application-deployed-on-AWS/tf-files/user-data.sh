3b16d7f66507b8575da47d873220c8ff452f152e
Mrt-GitHub <bern.murat@gmail.com>
2022-01-09 01:14:27 +0100

    all tf-files added, and user-data configured

---
+++ devops/202-Terraform-Phonebook-Application-deployed-on-AWS/tf-files/user-data.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+pip3 install flask_mysql
+yum install git -y
+cd /home/ec2-user && git clone https://github.com/Mrt-GitHub/My-Projects.git && rm -r My-Projects/!(devops)
+python3 /home/ec2-user/devops/202-Terraform-Phonebook-Application-deployed-on-AWS/phonebook-app.py
